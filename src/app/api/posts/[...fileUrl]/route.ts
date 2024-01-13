b7a88edf7764f6942b40e7bd3bf03f2a6d86d56b
Oscar Arranz <oscararranzp@gmail.com>
2024-01-13 03:45:33 +0100

    fix: search

--- src/app/api/posts/[...fileUrl]/route.ts
+++ src/app/api/posts/[...fileUrl]/route.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 import { getPost } from '../../../../utils/posts';
+import { getPublicFilePath } from '../../../../utils/file';
 
 export const GET = async (
   _request: Request,
   { params }: { params: { fileUrl: string[] } }
 ) => {
   const post = await getPost(
-    path.normalize(`./${params.fileUrl.join('/')}.mdx`)
+    getPublicFilePath(path.normalize(`./${params.fileUrl.join('/')}.mdx`))
   );
 
   return Response.json({ post });
